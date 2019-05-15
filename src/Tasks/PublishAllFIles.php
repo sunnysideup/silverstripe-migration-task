2698b541b4ea2a56380c4f8c77a642ecadb4718c
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-05-15 21:20:06 +1200

    PATCH: show message before doing it so that it is easier to debug

--- src/Tasks/PublishAllFIles.php
+++ src/Tasks/PublishAllFIles.php
@@ -6,6 +6,7 @@ use SilverStripe\Dev\BuildTask;
 use SilverStripe\Dev\Tasks\MigrateFileTask;
 use SilverStripe\Control\Director;
 use SilverStripe\Assets\File;
+use SilverStripe\Assets\Folder;
 use SilverStripe\AssetAdmin\Controller\AssetAdmin;
 use SilverStripe\ORM\DB;
 use SilverStripe\ORM\Queries\SQLSelect;
@@ -23,46 +24,50 @@ class PublishAllFiles extends MigrateDataTask
 
         $sqlQuery = new SQLSelect();
         $sqlQuery->setFrom('File');
+        $sqlQuery->setOrderBy('ParentID');
         $sqlQuery->selectField('ID');
         //$sqlQuery->addWhere(['ClassName' => 'SilverStripe\Assets\File']);
 
         // Execute and return a Query object
         $result = $sqlQuery->execute();
-
+        die(ASSETS_PATH);
         foreach ($result as $row) {
             $file = File::get()->byID($row['ID']);
-
             $name = $file->getFilename();
-            $originalDir = BASE_PATH . '/'.Director::publicDir().'/assets/';
+            if($file instanceof Folder) {
+                $this->flushNow('Skipping Folder: '.$name);
+            } else {
+                $originalDir = ASSETS_PATH.'/';
 
-            if(file_exists($originalDir.$name) && !is_dir($originalDir.$name)) {
-                if(!$file->getField('FileHash')) {
-                    $hash = sha1_file($originalDir.$name);
-                    DB::query('UPDATE "File" SET "FileHash" = \''.$hash.'\' WHERE "ID" = \''.$file->ID.'\' LIMIT 1;');
-                } else {
-                    $hash = $file->FileHash;
+                if(file_exists($originalDir.$name) && !is_dir($originalDir.$name)) {
+                    if(!$file->getField('FileHash')) {
+                        $hash = sha1_file($originalDir.$name);
+                        DB::query('UPDATE "File" SET "FileHash" = \''.$hash.'\' WHERE "ID" = \''.$file->ID.'\' LIMIT 1;');
+                    } else {
+                        $hash = $file->FileHash;
+                    }
+
+                    $targetDir = str_replace(
+                        './',
+                        '',
+                        ASSETS_PATH .'/.protected/'. dirname($name)
+                            .'/'. substr($hash, 0, 10) . '/'
+                    );
+
+
+                    if(!file_exists($targetDir)) {
+                        mkdir($targetDir, 0755, true);
+                    }
+
+                    rename($originalDir . $name, $targetDir . basename($name));
+
+
+                    $this->flushNow($originalDir . $name .' > '. $targetDir . basename($name), 'obsolete');
+                }else{
+                    $this->flushNow('Publishing: '.$name, 'created');
+                    $admin->generateThumbnails($file);
+                    $file->copyVersionToStage('Stage', 'Live');
                 }
-
-                $targetDir = str_replace(
-                    './',
-                    '',
-                    BASE_PATH . '/' . Director::publicDir() . '/assets/.protected/'. dirname($name)
-                        .'/'. substr($hash, 0, 10) . '/'
-                );
-
-
-                if(!file_exists($targetDir)) {
-                    mkdir($targetDir, 0755, true);
-                }
-
-                rename($originalDir . $name, $targetDir . basename($name));
-
-
-                $this->flushNow($originalDir . $name .' > '. $targetDir . basename($name), 'obsolete');
-            }else{
-                $admin->generateThumbnails($file);
-                $file->copyVersionToStage('Stage', 'Live');
-                $this->flushNow('Published: '.$name, 'created');
             }
 
             $file->destroy();
