ab414127a766321053aa2198a896fde43cdd0265
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2019-07-17 23:17:04 +1200

    PATCH: fixes

--- src/Tasks/CheckDataObjectStatics.php
+++ src/Tasks/CheckDataObjectStatics.php
@@ -26,6 +26,12 @@ class CheckDataObjectStatics extends MigrateDataTask
      *     ]
      * @var array
      */
+    private static $no_searchable_fields = [
+        'Sort',
+        'SortOrder',
+        'ID',
+        'Version',
+    ];
     private static $other_fields_to_check = [];
 
     protected $title = 'Data Object Statics Check';
@@ -33,6 +39,8 @@ class CheckDataObjectStatics extends MigrateDataTask
     protected $description = 'Goes through all Data Object classes and checks for missing statics';
 
     protected $enabled = true;
+
+    protected $objectClassNames = [];
 
     protected $listOfAllClasses = [];
 
@@ -47,28 +55,59 @@ class CheckDataObjectStatics extends MigrateDataTask
 
         // make a list of all classes
         // include baseclass = false
-        $objectClassNames = ClassInfo::subclassesFor(DataObject::class, false);
-        foreach ($objectClassNames as $objectClassName) {
+        $this->objectClassNames = ClassInfo::subclassesFor(DataObject::class, false);
+        foreach ($this->objectClassNames as $objectClassName) {
             $slashed = addslashes($objectClassName);
             $this->listOfAllClasses[$slashed] = ClassInfo::shortName($objectClassName);
         }
         $this->countsOfAllClasses = array_count_values($this->listOfAllClasses);
+        $this->summaryAndSearchableCheck();
+    }
 
+    protected function summaryAndSearchableCheck()
+    {
+        $errors = [];
+        $values = [];
         //check all classes
-        foreach ($objectClassNames as $objectClassName) {
+        foreach ($this->objectClassNames as $objectClassName) {
             if(count($this->onlyRunFor) && ! in_array($objectClassName, $this->onlyRunFor)) {
                 continue;
             }
 
-            $this->flushNow($objectClassName);
-            $summaryFields = Config::inst()->get('summary_fields');
-            $searchableFields = Config::inst()->get('searchable_fields');
-            if(count($summaryFields) && ! count($searchableFields)) {
-                $this->flushNow('... Error in '.$objectClassName.' there are summary fields, but no searchable fields', 'error');
+            $summaryFields = Config::inst()->get($objectClassName, 'summary_fields');
+            $searchableFields = Config::inst()->get($objectClassName, 'searchable_fields');
+            if(count($summaryFields) > 0 && count($searchableFields) === 0) {
+                $dbFields = array_keys(Config::inst()->get($objectClassName, 'db'));
+                if(count($dbFields) > 5) {
+                    $recommended = array_intersect($dbFields, $summaryFields);
+                    if(count($recommended) < 2) {
+                        $recommended = $dbFields;
+                    }
+                } else {
+                    $recommended = $dbFields;
+                }
+                if(count($recommended)) {
+                    foreach($recommended as $k=>$v){
+                        if (in_array($v, $this->Config()->no_searchable_fields)) {
+                            unset($recommended[$k]);
+                        }
+                    }
+                    $values[$objectClassName] = $objectClassName.':'.
+                        "\n  searchable_fields:\n    - ".
+                        implode("\n    - ", $recommended)
+                        ."\n\n";
+                    $this->flushNow('... Error in '.$objectClassName.' there are summary fields, but no searchable fields', 'error');
+                }
             } else {
-                $this->flushNow('... OK', 'created');
+                // $this->flushNow('... OK', 'created');
             }
         }
+        echo '<pre>';
+        echo "\n# Autocreated from: Sunnysideup-MigrateData-Tasks-CheckDataObjectStatics\n\n";
+        echo implode("", $values);
+        echo '</pre>';
+
     }
+
 
 }
