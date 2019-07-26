add943d95a790c12406a3e9788fd071e55fe9e88
thiemen <nt@sunnysideup.co.nz>
2019-07-26 15:54:56 +1200

    MINOR: adding new test

---
+++ src/Tasks/DraftVsLive.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Sunnysideup\MigrateData\Tasks;
+
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\Dev\Tasks\MigrateFileTask;
+use SilverStripe\Control\Director;
+use SilverStripe\Assets\File;
+use SilverStripe\Assets\Folder;
+use SilverStripe\AssetAdmin\Controller\AssetAdmin;
+use SilverStripe\ORM\DB;
+use SilverStripe\ORM\Queries\SQLSelect;
+use SilverStripe\Versioned\Versioned;
+use SilverStripe\Core\Injector\Injector;
+
+class DraftVsLive extends MigrateDataTask
+{
+    protected $title = 'Compare Drafts vs Live';
+
+    protected $description = 'Go through every table and compare DRAFT vs Live';
+
+    protected $selectedTables = [];
+
+    protected $missingColumns = [];
+
+    protected function performMigration()
+    {
+
+        //get tables in DB
+        $dbTablesPresent = [];
+        if(empty($this->selectedTables)){
+            $rows = DB::query('SHOW tables');
+            foreach ($rows as $row) {
+                $table = array_pop($row);
+                $dbTablesPresent[$table] = $table;
+            }
+        } else {
+            $dbTablesPresent = $this->selectedTables;
+        }
+        foreach($dbTablesPresent as $table) {
+            $this->missingColumns[$table] = [];
+            $liveTable = $table.'_Live';
+            if($this->tableExists($liveTable)) {
+
+                //check count
+                $draftCount = DB::query('SELECT COUNT(ID) FROM '.$table.' ORDER BY ID;')->value();
+                $liveCount = DB::query('SELECT COUNT(ID) FROM '.$liveTable.' ORDER BY ID;')->value();
+                if(intval($draftCount !== $liveCount)) {
+                    $this->flushNow(
+                        'TABLE '.$table.' count ('.$draftCount.')
+                        is not the same as count for '.$liveTable.' ('.$liveCount.'),
+                        DIFFERENCE: '.($draftCount - $liveCount).' more entries on DRAFT.',
+                        'deleted'
+                    );
+                } else {
+                    $this->flushNow('TABLE '.$table.' DRAFT and LIVE count is identical ...', 'created');
+                }
+
+                //check columns
+                $this->compareColumnsOuter($table, $liveTable);
+                $allOk = true;
+                $draftRows = DB::query('SELECT * FROM '.$table.' ORDER BY ID;');
+
+                //check rows
+                foreach($draftRows as $draftRow) {
+                    $allOk = $this->compareOneRow($draftRow, $table, $liveTable);
+                }
+                if($allOk) {
+                    $this->flushNow('... For matching rows, DRAFT and LIVE are identical.');
+                }
+            } else {
+                $this->flushNow('No Live version for '.$table);
+            }
+        }
+    }
+
+    protected function compareOneRow($draftRow, $table, $liveTable)
+    {
+        $allOk = true;
+        $liveRows = DB::query('SELECT * FROM '.$liveTable.' WHERE ID = '.$draftRow['ID'].';');
+        foreach($liveRows as $liveRow) {
+            $results = array_diff($draftRow, $liveRow) + array_diff($liveRow, $draftRow);
+            foreach($results as $key => $value) {
+                if(! isset($draftRow[$key])) {
+                    $draftRow[$key] = '???';
+                }
+                if(! isset($liveRow[$key])) {
+                    $liveRow[$key] = '???';
+                }
+                $allOk = false;
+                $this->flushNow(
+                    '... ... DRAFT !== LIVE for ID <strong>'.$draftRow['ID'].'</strong>, '.
+                    'FIELD: <strong>'.$key.'</strong>:
+                    ('.strip_tags(substr(print_r($draftRow[$key], 1), 0, 100)).')
+                    ('.strip_tags(substr(print_r($liveRow[$key], 1), 0, 100)).') === ',
+                    'deleted'
+                );
+            }
+        }
+
+        return $allOk;
+    }
+
+    protected function compareColumnsOuter($table, $liveTable, $backwards = false)
+    {
+        $draftRows1 = DB::query('SELECT * FROM '.$table.' ORDER BY ID LIMIT 1;');
+        $LiveRows1 = DB::query('SELECT * FROM '.$liveTable.' ORDER BY ID LIMIT 1;');
+        foreach($draftRows1 as $draftRow) {
+            foreach($LiveRows1 as $liveRow) {
+                $this->compareColumnsInner($draftRow, $liveRow, $table, false);
+            }
+        }
+    }
+    protected function compareColumnsInner($rowA, $rowB, $table, $backwards = false)
+    {
+        $tableA = 'DRAFT';
+        $tableB = 'LIVE';
+        if($backwards) {
+            $tableB = 'DRAFT';
+            $tableA = 'LIVE';
+        }
+        $result = array_diff_key($rowA,$rowB);
+        foreach($result as $key => $value) {
+            $this->missingColumns[$table][$key] = $key;
+            $this->flushNow(
+                '... Found a column in the '.$tableA.' table that did not match the '.$tableB.' table: '.$key,
+                'deleted'
+            );
+        }
+        if(! $backwards) {
+            $this->compareColumnsInner($rowB, $rowA, $table, true);
+        }
+    }
+
+}
