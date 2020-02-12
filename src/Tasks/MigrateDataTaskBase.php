ad67889f57db44fc6e86024dc3236910188b8a4b
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2020-02-12 21:07:01 +1300

    PATCH: cleanup

--- src/Tasks/MigrateDataTaskBase.php
+++ src/Tasks/MigrateDataTaskBase.php
@@ -398,48 +398,4 @@ abstract class MigrateDataTaskBase extends BuildTask
     }
 
 
-    /**
-     * Show a message about task currently running
-     *
-     * @param string $message to display
-     * @param string $type one of [created|changed|repaired|obsolete|deleted|error]
-     * @param boolean $bullet add a bullet to message?
-     *
-     **/
-    protected function flushNow($message, $type = '', $bullet = true)
-    {
-        if (is_array($message)) {
-            $message = print_r($message);
-        }
-        echo '';
-        // check that buffer is actually set before flushing
-        if (ob_get_length()) {
-            @ob_flush();
-            @flush();
-            @ob_end_flush();
-        }
-        @ob_start();
-        if (Director::is_cli()) {
-            $message = strip_tags($message);
-        }
-        if ($bullet) {
-            DB::alteration_message($message, $type);
-        } else {
-            echo $message;
-        }
-    }
-
-
-    /**
-     * Show a message about task currently running
-     *
-     * @param string $message to display
-     * @param string $type one of [created|changed|repaired|obsolete|deleted|error]
-     * @param boolean $bullet add a bullet to message?
-     *
-     **/
-    protected function flushNowLine()
-    {
-        $this->flushNow('--------------------------------------------------------');
-    }
 }
